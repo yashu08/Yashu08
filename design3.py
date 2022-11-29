65a9a966330bea6e0ac48b7050bf18d5f8b35f79
Yashu Narayan <118375764+Beeyou08@users.noreply.github.com>
2022-11-29 19:21:06 +0530

    Add files via upload

---
+++ design3.py
@@ -0,0 +1,11 @@
+import turtle 
+painter = turtle.Turtle()
+painter.pencolor("red")
+for i in range(45):
+    painter.forward(45)
+    painter.left(123)   
+painter.pencolor("purple")
+for i in range(50):
+    painter.forward(100)
+    painter.left(123)
+turtle.done()
